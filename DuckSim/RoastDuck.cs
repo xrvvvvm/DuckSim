22d9417d952615827ad099740e4e790f9755f90f
Maxim <oxxxyhot@gmail.com>
2021-03-10 19:26:07 +0500

    add roast duck, swim no way

---
+++ DuckSim/RoastDuck.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using DuckSim.behaviors;
+
+namespace DuckSim
+{
+    public class RoastDuck : Duck
+    {
+        public RoastDuck()
+        {
+            quackBehavior = new MuteQuack();
+            flyBehavior = new FlyNoWay();
+            swimBehavior = new SwimNoWay();
+        }
+        
+        public override void display()
+        {
+            Console.WriteLine("RoastDuck display");
+        }
+    }
+}
---
+++ DuckSim/behaviors/SwimNoWay.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using DuckSim.interfaces;
+
+namespace DuckSim.behaviors
+{
+    public class SwimNoWay : ISwimmable
+    {
+        public void swim()
+        {
+            Console.WriteLine("Swim no way");
+        }
+    }
+}
